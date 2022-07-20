a1a2415542d0c883cf7276d2c8a71f3140c761a5
Eliazith Zeledon <Eliazithzeledon27@gmail.com>
2022-07-20 02:23:27 -0600

    Revert "Agregamos reciclerView para la base de datos SQLite"
    This reverts commit f73501c5e0bf518851147401154fef4a0d09c541.

--- app/src/main/java/com/example/solicitudprestamo/RegistrosProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.solicitudprestamo
-
-class RegistrosProvider {
-    //Aqui mandamos a llamar a los registros de nuestra base de datos SQLite para luego utilizarlos.
-
-    companion object{
-        val listprestamos = listOf<Prestamos>()
-    }
-}
